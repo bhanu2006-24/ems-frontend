7d171e722413a05660a629fc7526e22168643c62
Jesse A Herrera <tuu07965@temple.edu>
2025-11-17 07:48:04 -0500

    Adding header and footer components to app, also adding custom css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import HeaderComponent from "./components/HeaderComponent.jsx";
 import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
 
 function App() {
 
     return (
         <>
+            <HeaderComponent/>
             <ListEmployeeComponent/>
+            <FooterComponent/>
         </>
     )
 }
