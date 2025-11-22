fe7d884c35652d800b8bf943c0541fb7c245fe5b
Jesse A Herrera <tuu07965@temple.edu>
2025-11-22 00:28:55 -0500

    Adding Delete Employee API (DELETE)

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {listEmployees} from "../services/EmployeeService.js";
+import {deleteEmployee, listEmployees} from "../services/EmployeeService.js";
 import {useNavigate} from "react-router-dom";
 
 /**
@@ -15,14 +15,20 @@ const ListEmployeeComponent = () => {
     // Hook to navigate between pages
     const navigator = useNavigate();
 
-    // Fetch employees when the component mounts
-    useEffect(() => {
+    // Function to fetch all employees from the backend.
+    // Must go before useEffect so ESLint doesn't complain
+    function getAllEmployees() {
         listEmployees().then((response) => {
             console.log("Employees fetched successfully: ", response.data);
             setEmployees(response.data)
         }).catch((error) => {
             console.error("Error fetching employees: ", error);
         });
+    }
+
+// Fetch employees when the component mounts
+    useEffect(() => {
+        getAllEmployees();
     }, []);
 
     // Function to add a new employee
@@ -36,7 +42,13 @@ const ListEmployeeComponent = () => {
     }
 
     function removeEmployee(id) {
-        //TODO: Implement delete employee functionality
+        console.log("Removing employee with ID: ", id);
+        deleteEmployee(id).then((response) => {
+            console.log("Employee removed successfully. Status: ", response.status);
+            getAllEmployees();
+        }).catch((error) => {
+            console.error("Error removing employee: ", error);
+        });
     }
 
     return (
